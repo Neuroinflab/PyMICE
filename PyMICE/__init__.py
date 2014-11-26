9587d18065a00cd01da0100b1feac90a13ef5b5f
Jakub Kowalski <jkowalski@xp-dev.com>
2014-11-26 11:40:08 +0100

    import works again

--- PyMICE/__init__.py
+++ PyMICE/__init__.py
@@ -10,7 +10,7 @@ Copyright (c) 2012-2014 Laboratory of Neuroinformatics. All rights reserved.
 from ._Data import hTime
 from ._Merger import Merger
 from ._Loader import Loader, convertTime
-from ._Metadata import Phases
+from ._Metadata import Phase
 __ID__ = 'SciRes_000150'
 __version__ = __ID__ + ' 0'
 __all__ = []
