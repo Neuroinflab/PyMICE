4069d42d71a97238732fc3e3ebe81f7449bbecea
Jakub Kowalski <jkowalski@xp-dev.com>
2015-02-20 11:12:29 +0100

    Log analysis classes moved to a separate module; ExcludeMouseData slightly refactored; LickometerLogAnalyzer and PresenceLogAnalyzer available directly from pymice package

--- pymice/__init__.py
+++ pymice/__init__.py
@@ -12,6 +12,7 @@ from .Data import Loader, Merger
 from ._Tools import hTime, convertTime
 from ._Metadata import Phase, ExperimentConfigFile
 from ._Results import ResultsCSV
+from .LogAnalyser import LickometerLogAnalyzer, PresenceLogAnalyzer
 __ID__ = 'nlx_158570'
 __version__ = __ID__ + ' 0'
 __all__ = []
