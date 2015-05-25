0cb51fec50b20482539dfe9aa8c66698bb19a320
Jakub Kowalski <jkowalski@xp-dev.com>
2015-05-25 10:22:59 +0200

    Licensing information provided

--- Auxilary/ICMerge.sh
+++ Auxilary/ICMerge.sh
@@ -1,4 +1,25 @@
 #!/bin/bash
+###############################################################################
+#                                                                             #
+#    PyMICE library                                                           #
+#                                                                             #
+#    Copyright (C) 2015 Jakub M. Kowalski (Laboratory of Neuroinformatics;    #
+#    Nencki Institute of Experimental Biology)                                #
+#                                                                             #
+#    This software is free software: you can redistribute it and/or modify    #
+#    it under the terms of the GNU General Public License as published by     #
+#    the Free Software Foundation, either version 3 of the License, or        #
+#    (at your option) any later version.                                      #
+#                                                                             #
+#    This software is distributed in the hope that it will be useful,         #
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of           #
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            #
+#    GNU General Public License for more details.                             #
+#                                                                             #
+#    You should have received a copy of the GNU General Public License        #
+#    along with this software.  If not, see http://www.gnu.org/licenses/.     #
+#                                                                             #
+###############################################################################
 if [ $# -eq 0 ] || [ $# -gt 3 ]
 then
   echo "Usage: $0 <session directory> [<archive directory> [<destination directory>]]"
--- pymice/LogAnalyser.py
+++ pymice/LogAnalyser.py
@@ -1,13 +1,27 @@
 #!/usr/bin/env python
 # encoding: utf-8
-"""
-LogAnalyser.py
+###############################################################################
+#                                                                             #
+#    PyMICE library                                                           #
+#                                                                             #
+#    Copyright (C) 2013-2015 Jakub M. Kowalski, S. Łęski (Laboratory of       #
+#    Neuroinformatics; Nencki Institute of Experimental Biology)              #
+#                                                                             #
+#    This software is free software: you can redistribute it and/or modify    #
+#    it under the terms of the GNU General Public License as published by     #
+#    the Free Software Foundation, either version 3 of the License, or        #
+#    (at your option) any later version.                                      #
+#                                                                             #
+#    This software is distributed in the hope that it will be useful,         #
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of           #
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            #
+#    GNU General Public License for more details.                             #
+#                                                                             #
+#    You should have received a copy of the GNU General Public License        #
+#    along with this software.  If not, see http://www.gnu.org/licenses/.     #
+#                                                                             #
+###############################################################################
 
-Created by Szymon Łęski on 2013-01-21.
-Refactored by Jakub Kowalski on 2015-02-20.
-
-Copyright (c) 2012-2015 Laboratory of Neuroinformatics. All rights reserved.
-"""
 import time
 from datetime import datetime
 from pytz import UTC
