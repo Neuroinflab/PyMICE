82a7e9611ec61ce88b87f29329a68701e27f595a
Jakub Kowalski <jkowalski@xp-dev.com>
2015-06-30 18:27:47 +0200

    a first test of ObjectBase for TDD provided

---
+++ pymice/_ObjectBase.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# encoding: utf-8
+###############################################################################
+#                                                                             #
+#    PyMICE library                                                           #
+#                                                                             #
+#    Copyright (C) 2012-2015 Jakub M. Kowalski, S. Łęski (Laboratory of       #
+#    Neuroinformatics; Nencki Institute of Experimental Biology)              #
+#                                                                             #
+#    This software is free software: you can redistribute it and/or modify    #
+#    it under the terms of the GNU General Public License as published by     #
+#    the Free Software Foundation, either version 3 of the License, or        #
+#    (at your option) any later version.                                      #
+#                                                                             #
+#    This software is distributed in the hope that it will be useful,         #
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of           #
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            #
+#    GNU General Public License for more details.                             #
+#                                                                             #
+#    You should have received a copy of the GNU General Public License        #
+#    along with this software.  If not, see http://www.gnu.org/licenses/.     #
+#                                                                             #
+###############################################################################
+
+
+class ObjectBase(object):
+  """
+  A base class for efficient onbject filtering.
+
+  >>> ob = ObjectBase()
+  >>> ob.get()
+  []
+  """
+  def __init__(self):
+    """
+    """
+    pass
+
+  def put(self, objects):
+    pass
+
+  def get(self):
+    pass
+
+if __name__ == '__main__':
+  import doctest
+  import collections
+  ClassA = collections.namedtuple('ClassA', ['a', 'b'])
+  ClassA = collections.namedtuple('ClassB', ['c', 'd'])
+
+  doctest.testmod(extraglobs={'ClassA': ClassA,
+                              'ClassB': ClassB})
