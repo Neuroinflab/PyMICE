67223697b1909537a1f711a4df11eb69a8c14f51
Kuba <jkowalski@xp-dev.com>
2015-07-04 17:46:18 +0200

    1st test of fixTimezones function passed

--- pymice/_FixTimezones.py
+++ pymice/_FixTimezones.py
@@ -22,5 +22,5 @@
 #                                                                             #
 ###############################################################################
 
-def fixTimezones():
+def fixTimezones(timepoints, sessionStart):
   pass
