d75075e0081813d7eb9b880c9e079dc25d90806b
Kuba <j.kowalski@nencki.gov.pl>
2015-10-01 11:57:46 +0200

    Version upgrade due to package signing

--- pymice/__init__.py
+++ pymice/__init__.py
@@ -35,7 +35,7 @@ from ._Results import ResultsCSV
 from .LogAnalyser import (LickometerLogAnalyzer, PresenceLogAnalyzer,
                           TestMiceData, DataValidator)
 __NeuroLexID__ = 'nlx_158570'
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 __ID__ = __NeuroLexID__ + ' ' + __version__
 __all__ = []
 
--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ else:
 cPymice = Extension('pymice._C', sources = ['pymice.cpp'])
 #install_requires = ['numpy']?
 setup(name = 'PyMICE',
-      version = '0.2.0',
+      version = '0.2.1',
       url = 'https://neuroinflab.wordpress.com/research/pymice/',
       description = 'PyMICE - a Python™ library for mice behavioural data analysis',
       long_description="""PyMICE is a Python™ library for mice behavioural data analysis.
