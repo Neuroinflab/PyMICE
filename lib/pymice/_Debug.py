e500f722ebe5452cd37db06b24729150df959946
jkowalski <jkowalski@xp-dev.com>
2016-04-07 11:06:43 +0200

    copyright header corrected

--- lib/pymice/_Debug.py
+++ lib/pymice/_Debug.py
@@ -21,7 +21,7 @@
 #    You should have received a copy of the GNU General Public License        #
 #    along with this software.  If not, see http://www.gnu.org/licenses/.     #
 #                                                                             #
-##############################################################################################################################################################
+###############################################################################
 
 import operator
 
