24ab696804fdb805d787b0fa6329f34e5876f592
Jakub Kowalski <j.kowalski@nencki.gov.pl>
2016-10-09 17:33:27 +0200

    Analysis object provided

---
+++ lib/pymice/_Analysis.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# encoding: utf-8
+###############################################################################
+#                                                                             #
+#    PyMICE library                                                           #
+#                                                                             #
+#    Copyright (C) 2016 Jakub M. Kowalski (Laboratory of Neuroinformatics;    #
+#    Nencki Institute of Experimental Biology of Polish Academy of Sciences)  #
+#                                                                             #
+#    This software is free software: you can redistribute it and/or modify    #
+#    it under the terms of the GNU General Public License as published by     #
+#    the Free Software Foundation, either version 3 of the License, or        #
+#    (at your option) any later version.                                      #
+#                                                                             #
+#    This software is distributed in the hope that it will be useful,         #
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of           #
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            #
+#    GNU General Public License for more details.                             #
+#                                                                             #
+#    You should have received a copy of the GNU General Public License        #
+#    along with this software.  If not, see http://www.gnu.org/licenses/.     #
+#                                                                             #
+###############################################################################
+
+from ._Ens import Ens
+
+class Analysis(object):
+    def __init__(self, **analysers):
+        self.__analysers = analysers
+
+    def __call__(self, objects):
+        return Ens({name: analyser(objects)
+                    for name, analyser in self.__analysers.items()})
