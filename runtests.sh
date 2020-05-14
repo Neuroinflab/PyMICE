9f89ae95845007d3af43c8ae0f5c31d9194ac00e
Jakub Dzik <abukaj@users.noreply.github.com>
2020-05-14 09:18:46 +0200

    Support only Python 3.5-3.8
    See also: c027098c7fe9618f331409fcb6953dabcaccaa7c,
              bafd1b3e5df61ae4c789c086fe430bbd45998a6b

--- runtests.sh
+++ runtests.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-source activate py27
-python -m unittest discover
-source deactivate
-source activate py33
-python -m unittest discover
-source deactivate
-source activate py34
-python -m unittest discover
-source deactivate
 source activate py35
+python -m unittest discover
+source deactivate
+source activate py36
+python -m unittest discover
+source deactivate
+source activate py37
+python -m unittest discover
+source deactivate
+source activate py38
 python -m unittest discover
 source deactivate
